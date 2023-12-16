7dc8b37b1b0a4b3d96af9a30f83fa93e3148ed9d
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-12-16 21:44:23 +0530

    "changes"

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,32 +1,30 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Items from '../api/itemsAPI';
 import { useLocation } from 'react-router-dom';
 
 const Item = (props) => {
-  const { image, description, price, category,setOrders,orders,id,btn,deleteOrder} = props;
+  const { image, description, price, category, setOrders, orders, id, btn, deleteOrder } = props;
 
 
   const location = useLocation();
-  useEffect(()=>
-  {
+  useEffect(() => {
 
-  },[location])
+  }, [location])
 
-  const placeOrder = ()=>
-  {
-    setOrders([...new Set(orders.concat(Items.filter((elem)=>{return elem.id===id})))]);
+  const placeOrder = () => {
+    setOrders([...new Set(orders.concat(Items.filter((elem) => { return elem.id === id })))]);
   }
 
   return (
     <>
       <div className="card mx-2 my-2">
-        <img src={image} className="card-img-top" alt="..." />
+        <img src={image} className="card-img-top w-50 h-50 m-auto" alt="..." />
         <div className="card-body">
           <h3 className="card-title">{category}</h3>
-          <p className="card-text" style={{fontSize: '12px'}}>{description}</p>
+          <p className="card-text" style={{ fontSize: '12px' }}>{description}</p>
           <h5 className="card-title">price : {price}/-</h5>
-          <button className="btn btn-primary my-2" style={{width:'100%'}} onClick={placeOrder}>{btn}</button>
-          <button className="btn btn-danger" style={{display:`${location.pathname === '/orders'?'block':'none'}`,width:"100%"}} onClick={()=>{deleteOrder(id)}}>Delete</button>
+          <button className="btn btn-primary my-2 w-100" style={{ display: `${location.pathname !== '/orders' ? 'block' : 'none'}` }} onClick={placeOrder}>{btn}</button>
+          <button className="btn btn-danger" style={{ display: `${location.pathname === '/orders' ? 'block' : 'none'}`, width: "100%" }} onClick={() => { deleteOrder(id) }}>Delete</button>
         </div>
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
-    const {orders} = props;
+    const { orders } = props;
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg" style={{backgroundColor:'cornflowerblue'}}>
+            <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'cornflowerblue' }}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand mx-4 fw-bold" to="/"  style={{color:'white'}}>AtoZ</Link>
+                    <Link className="navbar-brand mx-4 fw-bold" to="/" style={{ color: 'white' }}>AtoZ</Link>
                     <div>
                         <ul className='list-unstyled d-flex mb-0'>
                             <li>
-                                <Link aria-current="page" to="/"  style={{color:'white'}}><i className="fa-solid fa-house"></i></Link>
+                                <Link aria-current="page" to="/" style={{ color: 'white' }}><i className="fa-solid fa-house"></i></Link>
                             </li>
                             <li>
-                                <Link to="/orders" className='mx-5 '  style={{color:'white'}}><i className="fa-solid fa-cart-shopping"></i><span className={`badge bg-success ${orders.length===0?'d-none':''}`}>{orders.length}</span></Link>
+                                <Link to="/orders" className=' ms-5 me-2' style={{ color: 'white' }}><i className="fa-solid fa-cart-shopping"></i><span className={`badge bg-success ${orders.length === 0 ? 'd-none' : ''}`}>{orders.length}</span></Link>
                             </li>
                         </ul>
                     </div>
